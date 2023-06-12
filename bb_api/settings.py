d940bf125f709283a6cd3276ee7a0de8ade3e5f6
Johneriksson88 <john.e.eriksson@gmail.com>
2023-06-12 09:32:07 +0000

    Create superuser and requirements.txt

--- bb_api/settings.py
+++ bb_api/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-johneriksson88-bbapi-btpqxq1jf8k.ws-eu99.gitpod.io']
 
 
 # Application definition
