c28648d55b113c5e613711db895cbf0ca5fd7124
Johneriksson88 <john.e.eriksson@gmail.com>
2023-06-20 10:38:53 +0000

    Setup allowed hosts and client origin dev for react app

--- bb_api/settings.py
+++ bb_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -60,7 +61,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-johneriksson88-bbapi-btpqxq1jf8k.ws-eu100.gitpod.io', 'beatbuddies-api-a72df4dfc93e.herokuapp.com']
+ALLOWED_HOSTS = [
+    '8000-johneriksson88-bbapi-btpqxq1jf8k.ws-eu100.gitpod.io', 
+    os.environ.get('ALLOWED_HOST')
+    ]
 
 
 # Application definition
@@ -104,10 +108,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-     CORS_ALLOWED_ORIGINS = [
-         os.environ.get('CLIENT_ORIGIN')
-     ]
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+    ]
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
         r"^https://.*\.gitpod\.io$",
